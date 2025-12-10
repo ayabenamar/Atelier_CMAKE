5a0a5fac4edf6d107b77a53de860a4595198de3d
ayabenamar <aya.benamar94@gmail.com>
2025-12-10 09:29:47 +0100

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -5,6 +5,4 @@ int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
 int divide(int a, int b) { return a / b; }
 
-int car(int a) {
-    return a * a;   // ➜ carré
-}
+int car(int a) { return a * a; }
