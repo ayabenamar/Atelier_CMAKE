f3eed2bb1b241294664e02c50f2499a619fa7ffe
ayabenamar <aya.benamar94@gmail.com>
2025-12-10 09:14:56 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,10 @@
-double _add(double a, double b);
-double _sub(double a, double b);
-double _mul(double a, double b);
-double _div(double a, double b);
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divide(int a, int b);
+int car(int a);   // ➜ nouvelle fonction
+
+#endif
